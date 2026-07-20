0d7ef27a9e322b3fc82bc5316d95d11c54a1c76d
hungchi <hungchistudio@gmail.com>
2026-07-20 13:55:57 -0400

    move friend picker into vocabulary controller

--- hooks/useVocabularyController.ts
+++ hooks/useVocabularyController.ts
@@ -2,13 +2,16 @@
 
 import { useVocabularyPage } from "@/hooks/useVocabularyPage";
 import useVocabulary from "@/hooks/useVocabulary";
+import useVocabularyFriendPicker from "@/hooks/useVocabularyFriendPicker";
 
 export default function useVocabularyController() {
   const page = useVocabularyPage();
   const vocabulary = useVocabulary();
+  const friendPicker = useVocabularyFriendPicker();
 
   return {
     ...page,
     ...vocabulary,
+    ...friendPicker,
   };
 }
