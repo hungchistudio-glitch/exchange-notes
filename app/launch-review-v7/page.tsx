c61525a713fd7304c36f23234cf469612b82f6c6
hungchi <hungchistudio@gmail.com>
2026-08-12 20:12:39 -0400

    chore: establish V7 cinematic baseline from approved V6.2

---
+++ app/launch-review-v7/page.tsx
@@ -0,0 +1,9 @@
+import ExchangeNotesLaunchV7 from "@/components/launch-v7/ExchangeNotesLaunchV7";
+
+export default function LaunchReviewV7Page() {
+  return (
+    <main>
+      <ExchangeNotesLaunchV7 reviewMode />
+    </main>
+  );
+}
